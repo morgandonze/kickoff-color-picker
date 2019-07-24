bb6897495d38094d0daf4eed3025d7ae9e29cf5a
Connor Stauffer <connor.paul.stauffer@gmail.com>
2019-07-24 12:03:42 -0600

    Remove all trial stuff

--- server/migrations/20190724092050_populate_greetings.js
+++
@@ -1,7 +0,0 @@
-exports.up = function(knex) {
-  return knex("greetings").insert([{ body: "Welcome to Kudos Color Picker!" }]);
-};
-
-exports.down = function(knex) {
-  return knex("greetings").delete();
-};
--- server/migrations/20190724102738_create_palettes.js
+++
@@ -1,9 +0,0 @@
-exports.up = function(knex) {
-  return knex.schema.createTable("palettes", function(table) {
-    table.increments("id");
-  });
-};
-
-exports.down = function(knex) {
-  return knex.schema.dropTable("palettes");
-};
