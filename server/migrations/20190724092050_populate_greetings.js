ac7e2f2f751f940df8e8d437b84f5fa331f558cc
Connor <connor.paul.stauffer@gmail.com>
2020-07-29 08:08:59 -0600

    Upgrade dependencies

--- server/migrations/20190724092050_populate_greetings.js
+++ server/migrations/20190724092050_populate_greetings.js
@@ -1,7 +1,9 @@
-exports.up = function(knex) {
-  return knex("greetings").insert([{ body: "Welcome to Kudos Color Picker!" }]);
+exports.up = function (knex) {
+  return knex("greetings").insert([
+    { body: "Welcome to Kickoff Color Picker!" },
+  ]);
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex("greetings").delete();
 };
