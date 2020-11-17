64e6be561afa9ff77ca09f351b8c581779049ab4
Morgan Donze <donzemorgan@gmail.com>
2020-11-17 17:59:16 -0500

    Display selected color

--- client/components/color-picker/index.js
+++ client/components/color-picker/index.js
@@ -1,29 +1,52 @@
+import { useState, useEffect } from "react";
+
 const ChannelSlider = (props) => {
-  const { color, onChange } = props;
-  console.log(color)
+  const { color, onChange, value } = props;
 
   return (
     <div>
       <label>
         {color}:
-        <input name={color} type="range" onChange={onChange} />
+        <input
+          name={color}
+          type="range"
+          onChange={onChange}
+          min={0}
+          max={255}
+          value={value}
+        />
       </label>
-
     </div>
   );
 };
 
 const ColorPicker = () => {
-  const colorChangeHandler = (color) => (event) => {
-    console.log(color, event.nativeEvent)
-  }
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  const colorChangeHandler = (setter) => (event) => {
+    const value = event.nativeEvent.target.value;
+    setter(parseInt(value));
+  };
+
+  const redHex = red.toString(16).padStart(2, '0')
+  const greenHex = green.toString(16).padStart(2, '0')
+  const blueHex = blue.toString(16).padStart(2, '0')
+  const hexColor = `#${redHex}${greenHex}${blueHex}`
+  
+  console.log(hexColor)
 
   return (
     <div>
-      <div></div>
-      <ChannelSlider color="Red" onChange={colorChangeHandler("red")}/>
-      <ChannelSlider color="Green" onChange={colorChangeHandler("green")}/>
-      <ChannelSlider color="Blue" onChange={colorChangeHandler("blue")}/>
+      <div style={{
+          height: 100,
+          width: 100,
+          backgroundColor: hexColor
+      }} />
+      <ChannelSlider color="Red" onChange={colorChangeHandler(setRed)} value={red} />
+      <ChannelSlider color="Green" onChange={colorChangeHandler(setGreen)} value={green} />
+      <ChannelSlider color="Blue" onChange={colorChangeHandler(setBlue)} value={blue} />
     </div>
   );
 };
